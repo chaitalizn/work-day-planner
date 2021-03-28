6db60285576eb55f533190c2ed8ffb1eacd8bb72
chaitalizn <phchaitali3@gmail.com>
2021-03-28 15:24:39 -0400

    add colorcode to past, present and future time

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,27 @@
 //Display today's date
 var rightNow = moment().format('MMMM Do, YYYY - hh:mm A');
 $("#currentDay").html(rightNow);
+
+function hourTracker() {
+    var currentHour = moment().hour();
+    
+    //loop over all hours
+    $(".time-block").each(function() {
+        var colorCodeHour = parseInt($(this).attr("data-hour"));
+        
+        if (colorCodeHour < currentHour) {
+            $(this).addClass("past");
+        }
+        
+        if (colorCodeHour === currentHour) {
+            $(this).addClass("present");          
+        }
+
+        if (colorCodeHour > currentHour) {
+            $(this).addClass("future");
+        }
+    }
+    )
+}
+
+hourTracker();
